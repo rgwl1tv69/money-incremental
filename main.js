0abb5015a00f769e47f4a7ca6ae4e81b75154e31
rgwl1tv69 <168993357+rgwl1tv69@users.noreply.github.com>
2024-09-27 17:41:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+var money = new OmegaNum(1);
+var prestige = new OmegaNum(0);
+var moneygain = new OmegaNum(0);
+var u1 = new OmegaNum(0);
+var tps = 60;
+function totalPrestigeBoost() {
+  var prestigeBoost = prestige.add(1).pow(3);
+  return prestigeboost;
+}
+function totalU1Boost() {
+  var u1Boost = OmegaNum.pow(1.2,u1)
+  return u1Boost
+}
+function tick() {
+  var gain = new OmegaNum(1);
+  gain = gain.mul(totalPrestigeBoost());
+  gain = gain.mul(totalU1Boost());
+  money = money.add(gain)
+  document.getElementById("Money") = money.toString()
+}
+setInterval(tick,50);
